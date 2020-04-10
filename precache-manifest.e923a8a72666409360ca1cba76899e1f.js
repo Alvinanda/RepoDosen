7ac9e4c3c70bb5afd3d3f7365a472eb5da3d87fd
Alvinanda Rosha Kurniawan <alvinandakurniawan@yahoo.co.id>
2020-04-10 12:31:37 +0700

    yes
    ini

---
+++ precache-manifest.e923a8a72666409360ca1cba76899e1f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca81824a87dadce806245444a68a7e51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25c2d2f5aa0d7016ff77",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "df9326ea78420b1b4466",
+    "url": "/static/css/main.aac1561e.chunk.css"
+  },
+  {
+    "revision": "25c2d2f5aa0d7016ff77",
+    "url": "/static/js/2.e2a3a47e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.e2a3a47e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df9326ea78420b1b4466",
+    "url": "/static/js/main.e352499d.chunk.js"
+  },
+  {
+    "revision": "52e4cdf748d8b3d1cd4e",
+    "url": "/static/js/runtime-main.9fe15c4c.js"
+  },
+  {
+    "revision": "37137127297e0d252ff36189c3d37c57",
+    "url": "/static/media/Peteng2.37137127.png"
+  },
+  {
+    "revision": "bee7a5a092d66e30e21142dde0137a44",
+    "url": "/static/media/header.bee7a5a0.JPG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
